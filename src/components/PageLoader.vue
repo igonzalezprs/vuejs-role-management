a49b484eadced5c8c97a0529785daacd965a1db9
Isidro Gonzalez <igonzalezprs@gmail.com>
2021-11-27 14:05:58 -0400

    Add page loader component

---
+++ src/components/PageLoader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    v-if="active"
+    class="h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-gray-200 bg-opacity-50 transition-all"
+  >
+    <div class="flex justify-center items-center">
+      <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-accent" />
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  active: Boolean,
+});
+</script>
